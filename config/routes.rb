3c1d4bc315c398bb1d305e908bc5a75b46af782d
SimonBirrell <simon@eurodemand.com>
2015-05-31 13:15:28 +0100

    Sign in screen done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  #root to: 'visitors#index'
+
+#  devise_scope :user do
+#    root to: "devise/sessions#new"
+#  end
+#
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
   resources :users
   
 end
