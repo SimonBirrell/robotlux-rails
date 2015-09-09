284ff92dca638f1f835a92813fefe86d67c96201
Simon Birrell <manacoa@users.noreply.github.com>
2015-09-09 10:43:55 +0100

    Postmark setup

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,15 @@ module Robotlux
     
     # Simon Changes
     config.assets.paths << Rails.root.join("vendor","assets", "fonts")
+    ActionMailer::Base.smtp_settings = {
+      :port           => '25', # or 2525
+      :address        => ENV['POSTMARK_SMTP_SERVER'],
+      :user_name      => ENV['POSTMARK_API_TOKEN'],
+      :password       => ENV['POSTMARK_API_TOKEN'],
+      :domain         => 'robotlux.heroku.com',
+      :authentication => :cram_md5, # or :plain for plain-text authentication
+      :enable_starttls_auto => true, # or false for unencrypted connection
+    }
+    ActionMailer::Base.delivery_method = :smtp
   end
 end
