29c3e5ec69fd45388c18d2c035a9790d9ac4d895
Simon Birrell <manacoa@users.noreply.github.com>
2016-04-16 12:54:22 +0200

    Sign in error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     link_to_function (block_given? ? capture(&block) : args[0]), "$(this).closest('form').submit()", args.extract_options!
   end
     
+  def bootstrap_error_class(flash_key)
+  	flash_key_to_bootstrap_key = {'alert' => 'danger', 'notice' => 'info'}
+  	bootstrap_key = flash_key_to_bootstrap_key[flash_key] || flash_key
+  	"alert alert-#{bootstrap_key}"
+  end
+
 end
