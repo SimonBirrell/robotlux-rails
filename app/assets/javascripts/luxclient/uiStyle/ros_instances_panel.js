961e748c3e69bb2364e419dcee0f36e87c3ce166
Simon Birrell <manacoa@users.noreply.github.com>
2016-04-23 22:40:25 +0200

    revised ros instance panel

--- app/assets/javascripts/luxclient/uiStyle/ros_instances_panel.js
+++ app/assets/javascripts/luxclient/uiStyle/ros_instances_panel.js
@@ -9,6 +9,9 @@ var RosInstancesPanel = (function() {
 
     var Svg = null;
 
+    var NumberConnectedInstances = 0;
+    var ConnectedInstances = [];
+
     // Call this when you know what organization the user belongs to.
     // It will display as the title of the ROS Instances Panel.
     //  organization - string name of Organization
@@ -16,6 +19,20 @@ var RosInstancesPanel = (function() {
     module.setOrganization = function(organization) {
         d3.select("#connection-org").text(organization);
     };
+
+    function getPanelTitleFromInstances() {
+        var title = "";
+        for (var i=0; i<ConnectedInstances.length; i++) {
+            var instance = ConnectedInstances[i];
+            title = title + instance.rosInstanceHumanId + ", ";
+        }
+        if (title.length>0) {
+            title = title.substring(0, title.length - 2);
+        } else {
+            title = " ";
+        }
+        return title;
+    }
 
     // Call this functio whenever the list of ROS instances gains or loses members
     // Displays ROS Instance panel and makes items blink when appropriate
@@ -26,72 +43,146 @@ var RosInstancesPanel = (function() {
     //
     module.updateInstances = function(instances, displayInstance, hideInstance, dontBlinkOnEntry) {
         var blinkOnEnterDuration = ((dontBlinkOnEntry) ? 0 : 5000),
-            blinkOnExitDuration = 5000;
+            blinkOnExitDuration = 3000;
 
-        console.log("Updating ROS Instances panel");
+        var setNumberRosInstancesIndicator = function() {
+            // Update number of instances on top panel
+            $('#number-ros-instances-connected').text(instances.length.toString());
+
+            if (instances.length >0) {
+                $('#no-robots-detected').hide();
+                $('#number-ros-instances-connected').removeClass('danger');
+                $('#number-ros-instances-connected').removeClass('warning');
+                $('#number-ros-instances-connected').removeClass('success');
+                if (NumberConnectedInstances>0) {
+                    $('#number-ros-instances-connected').addClass('success');
+                } else {
+                    $('#number-ros-instances-connected').addClass('warning');
+                }
+            } else {
+                $('#number-ros-instances-connected').addClass('danger');
+                $('#number-ros-instances-connected').removeClass('warning');            
+                $('#number-ros-instances-connected').removeClass('success');            
+            }
+        };    
+
+        // Show 'no robots connected' message if number instances is zero
+        setNumberRosInstancesIndicator();
 
         var rosInstanceMenuItems = d3.select("#ros-instances-list")
-            .selectAll("li")
+            .selectAll(".ros-instance")
             .data(instances, function(d) {return d.rosInstanceId;});
 
         var rosInstanceMenuItemsEntering = rosInstanceMenuItems    
             .enter()
             .append("li")
-                .attr("class", "connection-new")
+                .attr("class", "ros-instance")
                 .on("click", function(d) {
-                    var inputTag = d3.select(this).select("input")[0][0];
-                    if ((!d.display)&&(inputTag.checked)) {
-                        d.display = true;
-                        displayInstance(d);
-                    } else if ((d.display)&&(!inputTag.checked)) {
-                        d.display = false;
-                        hideInstance(d);
-                    };
+                    var statusIcon = d3.select(this).select('.ros-instance-status'),
+                        chosenStatus = statusIcon.classed('chosen');   
+                    // If connection lost, choosing has no effect
+                    if (!statusIcon.classed('danger')) {
+                        chosenStatus = !chosenStatus;    
+                        statusIcon.classed('chosen', chosenStatus);
+                        if (chosenStatus) {
+                            statusIcon.classed('success', true);
+                            statusIcon.classed('hexagon', true);
+                            statusIcon.classed('warning', false);
+                            statusIcon.classed('outline-hexagon', false);
+                            ConnectedInstances.push(d);
+                            NumberConnectedInstances += 1;
+                            displayInstance(d);
+                        } else {
+                            statusIcon.classed('outline-hexagon', true);
+                            statusIcon.classed('hexagon', false);
+                            statusIcon.classed('success', false);
+                            statusIcon.classed('warning', false);
+                            var index = ConnectedInstances.indexOf(d);
+                            if (index > -1) {
+                                ConnectedInstances.splice(index, 1);
+                            }
+                            NumberConnectedInstances -= 1;
+                            hideInstance(d);
+                        }
+                        setNumberRosInstancesIndicator();
+                        // Set panel name
+                        $('#ros-space-panel-title').text(getPanelTitleFromInstances(instances));
+                        // Remove menu one second later (enough time to see instance selected)
+                        setTimeout(function() {
+                            $('#ros-connection-menu').removeClass('open', false);
+                        }, 1000);
+                    }
+                    // Selecting / de-selecting doesn't remove menu immediately
+                    d3.event.stopPropagation();
                 });
 
         rosInstanceMenuItemsEntering  
             .transition()
             .duration(blinkOnEnterDuration)
             .each('end', function(d) {
-                d3.select(this).attr("class", "connection-ok");
+                d3.select(this).attr("class", "ros-instance");
             });
 
-        rosInstanceMenuItemsEntering
-            .append("input")
-                .attr("type", "checkbox")
-                .attr("id", function(d) {return rosInstanceIdToCssId(d.rosInstanceId)});
+        // Connection link
+        // <a href="#">
+        var links = rosInstanceMenuItemsEntering            
+            .append("a")
+                .attr('href', '#');
 
-        var rosInstanceMenuItemsLabel = rosInstanceMenuItemsEntering
-            .append("label")
-                .attr("for", function(d) {return rosInstanceIdToCssId(d.rosInstanceId)});
+        links.each(function(d){
+                    // <div class="hexagon">
+                    d3.select(this).append('div')
+                        .attr('class', 'ros-instance-status hexagon warning')
+                        .each(function(d){
+                            // <i class="ion bug"></i>
+                            d3.select(this).append('span')
+                                .each(function(d){
+                                    d3.select(this).append('i')
+                                        .attr('class', 'ion-bug');
+                                })
+                        });
+                    // <span class="text_info">    
+                    d3.select(this).append('span')
+                        .attr('class', 'text_info')
+                        .text(function(d) {return d.rosInstanceHumanId; });
+                    // <span class="time_info">    
+                    d3.select(this).append('span')
+                        .attr('class', 'time_info')
+                        .text('Connecting...');                             
+                });
 
-        rosInstanceMenuItemsLabel
-            .append("span")
-            .text(" ");
-
-        rosInstanceMenuItemsLabel
-            .append("strong")
-            .attr("class", "connection-new blink")
-            //.text(function(d) {return d.rosInstanceId; })
-            .text(function(d) {return d.rosInstanceHumanId; })
-            .transition()
+        links.transition()
             .duration(blinkOnEnterDuration)
             .each('end', function(d) {
-                d3.select(this).attr("class", "connection-ok");
+                d3.select(this).selectAll('.time_info')
+                    .text("Connected ok");
+                var statusIcon = d3.select(this).select('.ros-instance-status'),
+                    chosenStatus = statusIcon.classed('chosen');   
+                if (!chosenStatus) {
+                    statusIcon.attr("class", "ros-instance-status outline-hexagon");
+                }    
             });
 
         var rosInstanceMenuItemsExiting = rosInstanceMenuItems   
             .exit();
 
+        // When connection is lost
         rosInstanceMenuItemsExiting
-            .attr("class", "connection-lost")
             .transition()
             .duration(blinkOnExitDuration)
+            .each('end', function(d) {
+                if (instances.length == 0) {
+                    $('#no-robots-detected').show();
+                }
+            })
             .remove();
 
         rosInstanceMenuItemsExiting
-            .selectAll("strong")
-                .attr("class", "connection-lost blink");
+            .selectAll(".ros-instance-status")
+                .attr("class", "ros-instance-status hexagon danger");
+        rosInstanceMenuItemsExiting
+            .selectAll(".time_info")
+                .text('Lost connection...');
 
     };
 
